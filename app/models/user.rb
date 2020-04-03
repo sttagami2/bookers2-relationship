ef7f5c4338c07815c869072ab2e498d2645d2c0a
vagrant <vagrant@localhost.localdomain>
2020-04-03 08:04:16 +0000

    [Update] お手上げ！

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,42 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  has_many :active_relationships, class_name: "Relationship",
+                  foreign_key: "follower_id",
+                  dependent: :destroy
+  # フォローしているユーザを一覧で表示（配列で取得)するための記述
+
+  has_many :passive_relationships, class_name: "Relationship",
+                  foreign_key: "followed_id",
+                  dependent: :destroy
+  # フォロワーの配列を展開するための記述
+
+  has_many :following, through: :active_relationships,source: :followed
+  # throughは1人に対していくつもの「フォローする/フォローされる」という多対多であるため使用
+  # following配列の元はfollowed idの集合であるという意味
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
+
+
+  # ユーザをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # ユーザをフォローする
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books do
-    resources :book_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
-    # ↑resourceが複数形になっていてidを上手く渡せないroute matchエラーが起きていた
-  end
+    resources :books do
+      resources :book_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+      # ↑resourceが複数形になっていてidを上手く渡せないroute matchエラーが起きていた
+    end
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
 
   root 'home#top'
   get 'home/about', to: 'home#about'
+  resources :relationships, only: [:create, :destroy]
 end
